f283a2388e1d8c873b93c5027b8848fb70ce25bc
Creed23R <kevinriosnicho@gmail.com>
2023-06-24 15:03:26 -0500

    Ejercicios 2 - OpenBootcamp js

---
+++ ejercicio_2.js
@@ -0,0 +1,13 @@
+const nombre = 'Kevin Jhoel';
+const edad = 22;
+const isDesarrollador = true;
+const fechNaci = new Date('2001-06-23')
+const libro = {
+    titulo: 'Juego de tronos',
+    autor: 'George R. R. Martin',
+    fecha: new Date(1996-8-6),
+    url: 'https://www.amazon.es/Juego-tronos-Canción-hielo-Éxitos/dp/8401032423/ref=sr_1_1?keywords=juego+de+tronos&qid=1687635779&s=books&sr=1-1'
+}
+
+const Kevin = {nombre,edad,isDesarrollador,fechNaci,libro};
+console.log(Kevin);
